f1028209bb20b5ead13038a00a5d957cded55b85
onethegarden <onethegarden@gmail.com>
2021-07-27 06:11:56 +0900

    blog 링크 연결

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,44 +1,61 @@
-import * as React from 'react'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/layout'
-import styled from 'styled-components'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import * as React from "react";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/layout";
+import styled from "styled-components";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const Article = styled.article`
- a{
-   color:black;text-decoration: none; 
- }
+  a {
+    color: black;
+    text-decoration: none;
+  }
 `;
 
 const BlogPage = ({ data }: any) => {
   return (
     <Layout pageTitle="Blog">
       <ul>
-        {
-          data.allMdx.nodes.map((node: any) => (
-            <Article key={node.id}>
-              <Link to="/detail">
-                <h2>{node.frontmatter.title}</h2>
-                <p>Posted: {node.frontmatter.date}</p>
+        {data.allMarkdownRemark.edges.map((edge: any) => {
+          return (
+            <Article key={edge.node.fields.slug}>
+              <Link to={edge.node.fields.slug}>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>Posted: {edge.node.frontmatter.date}</p>
               </Link>
             </Article>
-          ))
-        }
+          );
+        })}
       </ul>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
-query {
-  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-    nodes {
-      frontmatter {
-        date(formatString: "MMMM D, YYYY")
-        title
+  query {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM D, YYYY")
+            title
+          }
+        }
       }
     }
   }
-}
-`
-export default BlogPage
+`;
+// query {
+//   allMarkdownRemark {
+//     edges {
+//       node {
+//         fields {
+//           slug
+//         }
+//       }
+//     }
+//   }
+// }
+export default BlogPage;
