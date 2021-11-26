dd39bc9df54ca26e4f94066844f92eddb7abcb63
onethegarden <onethegarden@gmail.com>
2021-11-26 16:59:54 +0900

    mainpage UI변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import styled from "styled-components";
 import { graphql, Link } from "gatsby";
 import { useCategory } from "../hooks/useCategory";
@@ -36,36 +36,60 @@ const IndexPage = ({
     }) =>
       selectedCategory !== "All" ? categories.includes(selectedCategory) : true
   );
+  const [isCategory, setIsCategory] = useState(false);
+  const onClickToggleButton = () => {
+    setIsCategory(!isCategory);
+  };
 
   return (
     <Layout pageTitle="Blog">
-      <CategoryList
-        selectedCategory={selectedCategory}
-        categoryList={categoryList}
-      />
-      <PostUl>
-        {filteredPost.map((edge: Edge) => {
-          const { slug } = edge.node.fields;
-          const { title } = edge.node.frontmatter;
-          const { modifiedTime: date } = edge.node.parent;
-          return (
-            <Article key={slug}>
-              <Link to={slug}>
-                {edge.node.frontmatter.thumbnail?.childImageSharp && (
-                  <Img
-                    fluid={
-                      edge.node.frontmatter.thumbnail.childImageSharp.fluid
-                    }
-                    alt="Post Item Image"
-                  />
-                )}
-                <h2>{title}</h2>
-                <p>last updated: {date ? date : "-"}</p>
-              </Link>
-            </Article>
-          );
-        })}
-      </PostUl>
+      <Navigation>
+        <button
+          onClick={onClickToggleButton}
+          className={isCategory ? "" : "active"}
+        >
+          post
+        </button>
+        <span> | </span>
+        <button
+          onClick={onClickToggleButton}
+          className={isCategory ? "active" : ""}
+        >
+          categories
+        </button>
+      </Navigation>
+      {isCategory ? (
+        <CategoryList
+          selectedCategory={selectedCategory}
+          categoryList={categoryList}
+        />
+      ) : (
+        <PostUl>
+          {filteredPost.map((edge: Edge) => {
+            const { slug } = edge.node.fields;
+            const { title } = edge.node.frontmatter;
+            const { modifiedTime: date } = edge.node.parent;
+            return (
+              <Article key={slug}>
+                <Link to={slug}>
+                  {edge.node.frontmatter.thumbnail?.childImageSharp && (
+                    <Img
+                      fluid={
+                        edge.node.frontmatter.thumbnail.childImageSharp.fluid
+                      }
+                      alt="Post Item Image"
+                    />
+                  )}
+                  <Contents>
+                    <h2>{title}</h2>
+                    <p>{date ? date : "-"}</p>
+                  </Contents>
+                </Link>
+              </Article>
+            );
+          })}
+        </PostUl>
+      )}
     </Layout>
   );
 };
@@ -110,11 +134,9 @@ export interface Result {
 }
 
 export interface Data {
-  allMarkdownRemark: AllMarkdownRemark;
-}
-
-export interface AllMarkdownRemark {
-  edges: Edge[];
+  allMarkdownRemark: {
+    edges: Edge[];
+  };
 }
 
 export interface Edge {
@@ -139,6 +161,27 @@ export interface Edge {
     };
   };
 }
+const Navigation = styled.nav`
+  border-bottom: solid 1px #f5f5f5;
+  line-height: 30px;
+  span {
+    color: #b6b6b6;
+  }
+  button {
+    display: inline-block;
+    line-height: 30px;
+    letter-spacing: 2pt;
+    text-decoration: none;
+    color: #b6b6b6;
+    font-size: 12px;
+    font-size: 1.2rem;
+
+    &.active {
+      border-bottom: solid 2px #000;
+      color: #333337;
+    }
+  }
+`;
 
 const PostUl = styled.ul`
   margin-top: 3rem;
@@ -153,20 +196,25 @@ const PostUl = styled.ul`
 const Article = styled.article`
   h2 {
     margin: 0;
+    font-size: 1.2rem;
+  }
+  p {
+    font-size: 0.8rem;
   }
   a {
     color: black;
     text-decoration: none;
   }
-  border-radius: 1rem;
   margin: 1rem;
-  padding: 2rem 1rem;
   box-shadow: rgba(17, 17, 26, 0.05) 0px 4px 16px,
     rgba(17, 17, 26, 0.05) 0px 8px 32px;
   &:hover {
     transform: scale(1.1);
     transition-duration: 0.4s;
   }
+`;
+const Contents = styled.div`
+  padding: 2rem 1rem;
 `;
 
 export default IndexPage;
